e60c186cce618e0baeae18a2ff3089a5fc790f70
‘Xuan’ <2015441509@qq.com>
2024-01-26 06:11:58 +0800

    1.0
    本人最满意的一集

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 </div>
 
-*****本软件由纯Python制作而成，项目还在开发测试极度不成熟，推荐有一定代码基础的用户使用本软件*****
+*****本软件由纯Python制作而成，项目还在开发测试中极度不成熟，推荐有一定代码基础的用户使用本软件*****
 ## 免责声明
 
 本软件仅供学习交流使用，请于下载后24小时内删除，不得用于任何商业用途，否则后果自负。
@@ -15,29 +15,35 @@
 - [x] 监控弹幕
 - [x] 弹幕点歌
 - [x] 联动外部音乐播放器
-- [ ] 多线程实现同时监控弹幕及点歌
-- [ ] 歌单列表、歌词等等添加到OBS显示
+- [x] 多线程实现同时监控弹幕及点歌
+- [ ] 歌单列表、显示歌词等等
+- [ ] 支持更多音乐播放器
+- [ ] 制作UI 让配置以及启动软件更简单易上手
 
 ## 使用说明
 
 1. 安装[Python 3.11](https://www.python.org/downloads/release/python-3113/) (其他版本安装依赖项时可能会出现问题)
-2. 安装本软件:
+2. 安装[VLCforWindows](https://www.videolan.org/vlc/download-windows.html) (务必安装该软件,否则将无法使用本项目)
+3. 安装本软件:
    ```shell
    git clone https://github.com/xuan06zyx/bililive-vod.git
    cd bililive-vod
    ```
-3. 如果您是第一次运行本软件，还请先打开vod.py文件，在修改开头的配置参数(如下)：
+4. 如果您是第一次运行本软件，还请先打开bililive-vod.py文件，在修改开头的配置参数(如下)：
    ```text
    # 直播间ID
-   roomid = 'your-roomid'
+   roomid = 'your_roomid'
    # 外部音乐播放器暂停快捷键
    hotkey_start = 'ctrl', 'alt', 'p'
    # 外部音乐播放器下一首快捷键(可以跟暂停键一样)
    hotkey_next = 'ctrl', 'alt', 'right'
+   # 音量 0~100
+   volume = 50
    ```
-4. 国内用户可以输入`pip config set global.index-url https://mirrors.aliyun.com/pypi/simple` 设置国内pip源
-5. 输入`pip install -r requirements.txt`安装依赖
-6. 输入`Python vod.py`运行本软件
+5. 国内用户可以输入`pip config set global.index-url https://mirrors.aliyun.com/pypi/simple` 设置国内pip源
+6. 输入`pip install -r requirements.txt`安装依赖
+7. 输入`Python bililive-vod.py`运行本软件
+8. 在目标直播间输入点歌+歌曲名称，如`点歌青花瓷`即可
 
 ## 问题反馈
 
@@ -45,10 +51,7 @@
 2. 国内用户可以加我[QQ](https://api.lolimi.cn/API/tzmp/api.php?qq=2015441509)
 
 ## 注意事项
-1. 目前只支持高音质歌曲，即后缀为flac的，非flac后缀音乐（下载下来统一是flac，从歌曲链接中查看原后缀）会直接跳过
-2. 建议先播放外部音乐再启动本脚本
-3. 由于B站直播弹幕会有历史记录，如果历史记录中有点歌记录会直接触发点歌功能
-4. 由于本软件是单线程的，监听弹幕和播放音乐是同一个线程里的，播放音乐时不会再监听弹幕，所以在播放途中点歌了，点歌的弹幕不会被监听到，也就不会打断当前音乐的播放
-5. 实际上由于 pygame 和 API 的限制，标准及以下音质的音乐文件是 .m4a 的，而pygame 恰巧无法播放该格式的文件，所以我连自适应文件名后缀都没做XD
-
+1. 建议先播放外部音乐播放器再启动本脚本
+2. 由于B站直播弹幕会有历史记录，如果历史记录中最新一条弹幕含有点歌会直接触发点歌功能
+3. 本软件目前仅支持QQ音乐, 不支持以歌名+歌手的形式搜索指定歌曲
 ⭐**如果喜欢，点个star~**⭐
---
+++ bililive-vod.py
@@ -0,0 +1,151 @@
+import json
+import time
+import re
+import threading
+import requests
+import pyautogui
+import vlc
+
+# 直播间ID
+roomid = 'your_roomid'
+# 外部音乐播放器暂停快捷键
+hotkey_start = 'ctrl', 'alt', 'p'
+# 外部音乐播放器下一首快捷键(可以跟暂停键一样)
+hotkey_next = 'ctrl', 'alt', 'right'
+# 音量 0~100
+volume = 50
+
+# 直播间地址(修改roomid即可)
+bilibili_url = f'https://api.live.bilibili.com/xlive/web-room/v1/dM/gethistory?roomid={roomid}&room_type=0'
+# 浏览器UA
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+}
+
+# 变量声明(普通用户不需要管)
+# 弹幕日志
+item = []
+# 文本列表, 用于点歌功能读弹幕
+text_list = []
+# 上一首歌
+last_song = ''
+# 歌曲链接
+song_url = None
+
+# 创建vlc播放器实例
+instance = vlc.Instance()
+# 创建媒体播放器
+player = instance.media_player_new()
+
+
+def get_barrage():
+    global text_list
+    while True:
+        # 获取弹幕
+        bilibili = json.loads(requests.get(url=bilibili_url, headers=header).text)
+        bilibili = bilibili['data']['room']
+        for i in bilibili:
+            # 弹幕文本
+            text = i['text']
+            # 用户名
+            name = i['nickname']
+            # 发送时间
+            timeline = i['timeline']
+            barrage = f'[{timeline}] {name}: {text}'
+            if barrage not in item:
+                item.append(barrage)
+                text_list.append(text)
+                print(barrage)
+        time.sleep(0.5)
+
+
+def play_music():
+    global last_song, song_url
+    while '点歌' in text_list[-1]:
+        # 点歌功能
+        # 匹配点歌关键词
+        song_name = re.search(r'点歌\s*(.*)', text_list[-1]).group(1)
+
+        # 获取歌曲信息
+        qqmusic_url = f'https://api.lolimi.cn/API/yiny/?n=1&word={song_name}'
+        qqmusic = requests.get(url=qqmusic_url, headers=header).json()
+        # 歌名
+        song = qqmusic['data']['song']
+        # 通过歌名判断是否是与上次播放的是同一首歌
+        if song == last_song:
+            continue
+        # 歌手
+        song_singer = qqmusic['data']['singer']
+        # 专辑
+        song_album = qqmusic['data']['album']
+        # 发布时间
+        song_time = qqmusic['data']['time']
+        # 音质
+        song_quality = qqmusic['data']['quality']
+        # 歌曲时长
+        song_interval = qqmusic['data']['interval']
+        # 歌曲链接
+        song_url = qqmusic['data']['url']
+        # 留作下一次触发点歌使用
+        last_song = song
+        # 如果点歌机触发时有歌曲在播放, 先停止
+        if player.is_playing():
+            player.stop()
+        print('收到点歌请求:', song_name)
+        print('\n歌名:', song,
+              '\n歌手:', song_singer,
+              '\n音质:', song_quality,
+              '\n专辑:', song_album,
+              '\n时长:', song_interval,
+              '\n发布时间:', song_time,
+              )
+        time.sleep(0.8)
+
+
+def hk_next():
+    global song_url
+    while True:
+        if song_url:
+            print('歌曲链接:', song_url)
+            # 加载音乐文件
+            media = instance.media_new(song_url)
+            player.set_media(media)
+            # 设置音量
+            player.audio_set_volume(volume)
+            # 按下快捷键暂停外部音乐播放
+            pyautogui.hotkey(hotkey_start)
+            print('(已暂停外部音乐播放...)')
+            # 播放音乐
+            player.play()
+            time.sleep(1)
+            # 等待音乐播放完毕
+            while player.is_playing():
+                time.sleep(1)
+            # 按下快捷键恢复外部音乐播放
+            pyautogui.hotkey(hotkey_next)
+            print('(已恢复外部音乐播放...)')
+            song_url = None
+        time.sleep(0.8)
+
+
+# 创建监听弹幕线程并启动
+get_barrage = threading.Thread(target=get_barrage)
+get_barrage.start()
+
+# 等待一段时间, 确保弹幕信息已加载完毕
+time.sleep(1)
+
+# 创建点歌机线程并启动
+play_music = threading.Thread(target=play_music)
+play_music.start()
+
+# 等待一段时间, 确保歌曲信息已加载完毕
+time.sleep(1)
+
+#  创建播放歌曲线程并启动
+hk_next = threading.Thread(target=hk_next)
+hk_next.start()
+
+get_barrage.join()
+play_music.join()
+hk_next.join()
